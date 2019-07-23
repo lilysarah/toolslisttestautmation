e4b7183f59f9e1f532b83b3dafebc04d16d4bd20
student <ivaughan@highwirepress.com>
2019-07-23 13:50:51 +0100

    commit my dog colour

---
+++ src/test/java/co/uk/safebear/kennels/Dog.java
@@ -0,0 +1,16 @@
+package co.uk.safebear.kennels;
+
+public class Dog {
+    private int age;
+    private String colour;
+    boolean isNeutered;
+    boolean isFriendly;
+
+    public void setColour(String setColour) {
+        colour = setColour;
+    }
+    public String getColour(){
+        return colour;
+    }
+}
+
---
+++ src/test/java/co/uk/safebear/testdog.java
@@ -0,0 +1,19 @@
+package co.uk.safebear;
+
+import co.uk.safebear.kennels.Dog;
+import org.junit.Test;
+
+public class testdog {
+    @Test
+    public void testDog() {
+        Dog bruno = new Dog();
+        Dog fido = new Dog();
+
+        bruno.setColour("brown");
+        fido.setColour("white");
+
+
+
+    }
+}
+//mut be inside curly brackets to be run
